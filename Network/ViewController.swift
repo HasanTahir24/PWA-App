777fd7c8b93ed2aba759672b5c2e3286c675c1ba
RehanaSyed <rsyed5146@gmail.com>
2020-02-06 13:27:38 +0500

    Initial Commit

---
+++ Network/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Network
+//
+//  Created by Rehana Syed on 06/02/2020.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
